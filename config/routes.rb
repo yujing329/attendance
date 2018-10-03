d7b6aa640b8c1998f6f499e5a98b7ee12fde7fcb
YujiMatsumoto <yujing.329@gmail.com>
2018-10-03 06:21:24 +0000

    routes.rbのusers#createをコメントアウトから復帰

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  #post '/signup',  to: 'users#create'   #7.26: ユーザー登録のルーティングにPOSTリクエストを追加(リスト8.2で削除)
+  post '/signup',  to: 'users#create'   #7.26: ユーザー登録のルーティングにPOSTリクエストを追加(リスト8.2で削除)
   get    '/login',   to: 'sessions#new'     #8.2: リソースを追加して標準的なRESTfulアクションをgetできるようにする
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
