c7d4b716fcb8a01822c194ce3e7f6d289b1143dc
YujiMatsumoto <yujing.329@gmail.com>
2018-10-04 01:07:52 +0000

    Implement basic login(users_login_test.rb修正)

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,29 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+
+  test "login with invalid information" do  #8.9: フラッシュメッセージの残留をキャッチするテスト
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information" do  #8.23: 有効な情報を使ってユーザーログインをテスト
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
   test "login with valid information followed by logout" do  #8.31: ユーザーログアウトのテスト
     get login_path
     post login_path, params: { session: { email:    @user.email,
@@ -25,4 +48,5 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
 end
