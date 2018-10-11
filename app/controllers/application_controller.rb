2cc39c25505846b7db994118a5c548c708fdb508
YujiMatsumoto <yujing.329@gmail.com>
2018-10-11 14:14:13 +0000

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include SessionsHelper  #8.13: ApplicationコントローラにSessionヘルパーモジュールを読み込む
-  def hello
-    render html: "hello, world!"
-  end
+  include SessionsHelper
+
+  private
+
+    # ユーザーのログインを確認する
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -47,32 +47,23 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  private   #7.19: createアクションでStrong Parametersを使う
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
 
-  # beforeアクション
+  # beforeフィルター
 
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user    #10.15: beforeフィルターにlogged_in_userを追加
-    unless logged_in?
-    store_location      #10.31: ログインユーザー用beforeフィルターにstore_locationを追加
-    flash[:danger] = "Please log in."
-    redirect_to login_url
-    end
-  end
-  
-  # 正しいユーザーかどうか確認
-  def correct_user    #10.25: beforeフィルターを使って編集/更新ページを保護
+  # 正しいユーザーかどうかを確認
+  def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
-  
-  # 管理者かどうか確認
-  def admin_user      #10.59: beforeフィルターでdestroyアクションを管理者だけに限定
+
+  # 管理者かどうかを確認
+  def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
 end
