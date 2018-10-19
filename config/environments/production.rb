29192fa9f1c69cf8e5a59ae8514874a88c605c60
YujiMatsumoto <yujing.329@gmail.com>
2018-10-19 07:40:28 +0000

    SendGridのhost名変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'boiling-shelf-56098.herokuapp.com'
+  host = 'baseball-lovers.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
