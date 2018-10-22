30785255efa1495e5ea608d5557698cdba37566b
YujiMatsumoto <yujing.329@gmail.com>
2018-10-22 07:12:58 +0000

    新規登録とパスワード再設定メールの日本語化(textはそのまま)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "登録メール確認"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
+    mail to: user.email, subject: "パスワード再設定"
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,7 +6,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:michael)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "登録メール確認", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
     assert_match user.name,               mail.body.encoded
@@ -18,7 +18,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:michael)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
-    assert_equal "Password reset", mail.subject
+    assert_equal "パスワード再設定", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
     assert_match user.reset_token,        mail.body.encoded
